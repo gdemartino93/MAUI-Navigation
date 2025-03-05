9b059e7a6ac10e94b556de828d38bebdc476a3c0
gdemartino93 <g.demartino9393@gmail.com>
2025-03-06 00:21:01 +0100

    disabled button back

--- Views/AgreementPage.xaml.cs
+++ Views/AgreementPage.xaml.cs
@@ -6,7 +6,12 @@ public partial class AgreementPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
+    #region Event Override
+    protected override bool OnBackButtonPressed()
+    {
+        return true;
+    }
+    #endregion
     private void Button_Clicked_Accept(object sender, EventArgs e)
     {
 		Navigation.PopModalAsync(true);
